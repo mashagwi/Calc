27d5ff3a9117b9e7d5fbcf907c5aae29ef28d58d
Masha Gwin <mashagwi@si-a4.nsk.21-school.ru>
2023-09-26 15:30:29 +0700

    26/09/23

--- src/is_digit.c
+++
@@ -1,5 +0,0 @@
-int is_digit(char *str) {
-  int res = 0;
-  if (*str >= '0' && *str <= '9') res = 1;
-  return res;
-}
--- src/is_letter.c
+++
@@ -1,8 +0,0 @@
-#include "s21_smartcalc.h"
-
-int is_letter(char *str) {
-  int res = 0;
-  if (*str >= 'a' && *str <= 'z') res = 1;
-
-  return res;
-}
